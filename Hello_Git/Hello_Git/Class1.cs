2d11cbb404747e6ba5f49e045fabb2bb11dcb500
MarcoTurchiuli <marcotrc@hotmail.it>
2024-09-18 17:23:32 +0200

    primo commit

---
+++ Hello_Git/Hello_Git/Class1.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Hello_Git
+{
+	/// <summary>
+	/// Descrizione di riepilogo per Class1.
+	/// </summary>
+	class Class1
+	{
+		/// <summary>
+		/// Il punto di ingresso principale dell'applicazione.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Console.Write("ciao git!!");
+		}
+	}
+}
