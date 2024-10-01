710337eb3f4ea60699e2971aba82be06233c5bc6
MarcoTurchiuli <marcotrc@hotmail.it>
2024-10-01 10:10:55 +0200

    test comando amend

--- Hello_Git/Hello_Git/Class1.cs
+++ Hello_Git/Hello_Git/Class1.cs
@@ -13,15 +13,14 @@ namespace Hello_Git
 		[STAThread]
 		static void Main(string[] args)
 		{
-			//modificato il commento, modificato il valore di i e rimosso l'ultimo commento
-			//Questo commento MT
 			int i = 35; 
 			Console.Write("ciao git!! test2");
 			Login login = new Login();
 			Console.ReadLine();
 			
-			//nuove modifiche di marco che hanno valorizzato i a 10
-			//nuovo commento di prova
+			
+			//COMMIT ERRATA CHE VOGLIO ELIMINARE PRIMA DEL PUSH
+			//amend della commit
 		}
 	}
 }
