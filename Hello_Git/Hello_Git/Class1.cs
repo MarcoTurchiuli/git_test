c75865895295050b12728c458ff9d4cc2429412b
MarcoTurchiuli <marcotrc@hotmail.it>
2024-10-02 12:10:26 +0200

    rimozione commenti

--- Hello_Git/Hello_Git/Class1.cs
+++ Hello_Git/Hello_Git/Class1.cs
@@ -17,11 +17,6 @@ namespace Hello_Git
 			Console.Write("ciao git!! test2");
 			Login login = new Login();
 			Console.ReadLine();
-			
-			
-			//COMMIT ERRATA CHE VOGLIO ELIMINARE PRIMA DEL PUSH
-			
-			//TEST RIMOZIONE COMMIT DA REPO REMOTO
 		}
 	}
 }
